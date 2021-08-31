1991389f92f157506baba868bfe4b0cace8eda01
kannadurdiev <kolyabek13@gmail.com>
2021-08-31 20:20:45 +0500

    added style sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Menu } from '../Menu/Menu';
 import { SidebarProps } from './Sidebar.props';
+import Logo from '../logo.svg';
+import cn from 'classnames';
+import styles from './Sidebar.module.css';
 
-export const Sidebar = ({...props}:SidebarProps):JSX.Element => {
+export const Sidebar = ({className,...props}:SidebarProps):JSX.Element => {
     return (
-        <div {...props}>
+        <div {...props} className={cn(className, styles.sidebar)}>
+            <Logo className={styles.logo}/>
+            <div>поиск</div>
             <Menu/>
         </div>
     );
